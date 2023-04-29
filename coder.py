743532dd524fff924c97d6a9764df3fa343ea9c6
Egor Sokolov <faositb at gmail (.com)>
2023-04-29 18:34:56 +0300

    Добавлена работа с пакетами (кодирование, декодирование, проверка целостности)

--- coder.py
+++ coder.py
@@ -1,5 +1,41 @@
 import zlib
 
+
+def encode_packages(str, chars_count):
+    '''
+    Создаёт пакеты из строки str по chars_count символов,
+    переводит их в двоичный код и возвращает спискок из
+    кодов и хешей для каждого пакета
+    '''
+    encoded_packages = []
+    i = 0
+    while i < len(str):
+        encoded_packages.append(encode(str[i:i+chars_count]))
+        i += chars_count
+    return encoded_packages
+
+
+def decode_packages(encoded_packages):
+    '''
+    Декодирует пакеты, попутно вычисляя хеши для каждого полученного
+    пакета. Возвращает список из декодированных сообщений и хешей, которые
+    у них ДОЛЖНЫ быть (если пакет передан без искажений)
+    '''
+    decoded_packages_list = []
+    error_list = []
+    for each in encoded_packages:
+        decoded_package = [decode(each[0])[0], each[1]]
+        decoded_packages_list.append(decoded_package)
+    return decoded_packages_list
+
+
+def check_package_correctness(one_package):
+    '''
+    Проверка целостности пакета
+    '''
+    return (zlib.crc32(''.join(one_package[0]).encode('utf-8'))) == one_package[1]
+
+   
 def encode(str):
     '''
     Переводит строку str в список бит (на каждый символ по 7 бит)
@@ -9,7 +45,8 @@ def encode(str):
         symbol_code = bin(ord(symbol))[2::].zfill(7)
         for bit in symbol_code:
             bit_list.append(int(bit))
-    return (bit_list, zlib.crc32(str.encode('utf-8')))
+    return [bit_list, zlib.crc32(str.encode('utf-8'))]
+
 
 def decode(bit_list):
     '''
@@ -28,7 +65,22 @@ def decode(bit_list):
         codec = 64
     return (result_list, zlib.crc32(''.join(result_list).encode('utf-8')))
 
-def check_correctness(encoder_list, decoder_list):
+
+def find_wrong_packages(dec_message):
+    '''
+    Поиск искажённых пакетов
+    '''
+    error_list = []
+    for i in range(len(dec_message)):
+        if not check_package_correctness(dec_message[i]):
+            error_list.append(i)
+    return error_list
+
+
+def DEBUG_check_correctness(encoder_list, decoder_list):
+    '''
+    Для отладки: проверка хешей сообщений
+    '''
     if encoder_list[1] == decoder_list[1]:
         print('Сообщение не было искажено')
     else:
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import coder
 
-enc = coder.encode("Hello!")
+'''
+Пример работы достаточно низкоуровневых подпрограмм
+(работают не с пакетами, а просто со строками) из модуля coder
+'''
+enc = coder.encode("Hello")
 print(enc[0])
 dec = coder.decode(enc[0])
 print(dec[0])
-coder.check_correctness(enc, dec)
+coder.DEBUG_check_correctness(enc, dec)
 
 '''
 А теперь изказим один бит 
@@ -12,4 +16,30 @@ coder.check_correctness(enc, dec)
 enc[0][6] = 1
 dec = coder.decode(enc[0])
 print(dec[0])
-coder.check_correctness(enc, dec)
+coder.DEBUG_check_correctness(enc, dec)
+
+
+'''
+Пример более высокоуровневой работы
+(уже с пакетами) с подпрограммами из модуля coder
+
+'''
+initial_message = "Hello! Russia is the biggest country in the world. It is wached by several oceans. 1234567890"
+print('Исходное сообщение:\n%s' % initial_message)
+enc_message = coder.encode_packages(initial_message, 10)
+# print(enc_message)
+'''
+а теперь немного исказим сообщение enc_message
+'''
+enc_message[1][0][35] = not enc_message[1][0][35]
+enc_message[3][0][8] = not enc_message[3][0][8]
+
+dec_message = coder.decode_packages(enc_message)
+# print(dec_message)
+print('Декодированное сообщение:\n%s' % ''.join(''.join(each[0]) for each in dec_message))
+
+error_list = coder.find_wrong_packages(dec_message)
+if len(error_list) == 0:
+    print('Ошибок при передаче данных не было')
+else:
+    print('Ошибки в следующих номерах пакетов:',error_list)
